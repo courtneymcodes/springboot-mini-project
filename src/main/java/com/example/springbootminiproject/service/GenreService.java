23a80d4793be7d274ffc80b5d2097f2a1e9e0494
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 14:56:52 -0400

    add getGenres method to GenreService

--- src/main/java/com/example/springbootminiproject/service/GenreService.java
+++ src/main/java/com/example/springbootminiproject/service/GenreService.java
@@ -1,8 +1,11 @@
 package com.example.springbootminiproject.service;
 
+import com.example.springbootminiproject.model.Genre;
 import com.example.springbootminiproject.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GenreService {
@@ -11,4 +14,9 @@ public class GenreService {
     public void setGenreRepository(GenreRepository genreRepository) {
         this.genreRepository = genreRepository;
     }
+
+    public List<Genre> getGenres() {
+        return genreRepository.findAll();
+    }
+
 }
