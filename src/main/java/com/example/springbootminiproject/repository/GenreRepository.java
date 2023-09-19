20a49c37f007540923c89848cf2dafa65847f2a0
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 14:06:45 -0400

    add javadoc to GenreRepository

--- src/main/java/com/example/springbootminiproject/repository/GenreRepository.java
+++ src/main/java/com/example/springbootminiproject/repository/GenreRepository.java
@@ -4,6 +4,7 @@ import com.example.springbootminiproject.model.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**GenreRepository interface extends JpaRepository enabling it to inherit methods used to perform CRUD (Create, Read, Update, Delete) operations on entities of type Genre in the database*/
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
