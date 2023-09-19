b09bf63668114404f67605466893476fc2465b6c
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 09:30:53 -0400

    add primary key auto-generation to id field in User and Genre models

--- src/main/java/com/example/springbootminiproject/model/User.java
+++ src/main/java/com/example/springbootminiproject/model/User.java
@@ -1,15 +1,13 @@
 package com.example.springbootminiproject.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
     @Id
     @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column
     private String emailAddress;
