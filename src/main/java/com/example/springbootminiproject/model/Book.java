70e635b0c2c36d5e2c17e42297b8f47621149153
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 09:38:27 -0400

    add fields to Book model

--- src/main/java/com/example/springbootminiproject/model/Book.java
+++ src/main/java/com/example/springbootminiproject/model/Book.java
@@ -1,4 +1,11 @@
 package com.example.springbootminiproject.model;
 
+import java.time.LocalDate;
+
 public class Book {
+    private Long id;
+    private String author;
+    private String title;
+    private int publicationDate;
+    private String summary;
 }
