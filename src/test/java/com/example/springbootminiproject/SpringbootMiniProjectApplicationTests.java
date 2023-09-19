7726866ddc6c3a491a691e75d151213d0241526a
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 02:44:41 -0400

    create Spring Boot project

---
+++ src/test/java/com/example/springbootminiproject/SpringbootMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMiniProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
