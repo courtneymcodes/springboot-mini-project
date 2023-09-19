f5a941c37150f72e146da9a520c797eff9f02638
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 16:46:53 -0400

    add createGenre method to GenreService

--- src/main/java/com/example/springbootminiproject/service/GenreService.java
+++ src/main/java/com/example/springbootminiproject/service/GenreService.java
@@ -1,11 +1,13 @@
 package com.example.springbootminiproject.service;
 
+import com.example.springbootminiproject.exception.InformationExistsException;
 import com.example.springbootminiproject.model.Genre;
 import com.example.springbootminiproject.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GenreService {
@@ -21,6 +23,21 @@ public class GenreService {
      */
     public List<Genre> getGenres() {
         return genreRepository.findAll();
+    }
+
+    /**
+     *Saves an genre object to the database when a POST request is made to /genres endpoint
+     * @param genreObject from request body
+     * @return category object to save to the database
+     * @throws InformationExistsException if a category with same id already exists in the database
+     */
+    public Genre createGenre(Genre genreObject){
+        Optional<Genre> genreOptional = genreRepository.findById(genreObject.getId());
+        if(genreOptional.isEmpty()){
+            return genreRepository.save(genreObject);
+        } else {
+            throw new InformationExistsException("Genre with name " + genreObject.getName() + " already exists");
+        }
     }
 
 }
