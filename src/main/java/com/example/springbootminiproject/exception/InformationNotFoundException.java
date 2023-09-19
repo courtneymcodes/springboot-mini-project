54c68c80aba7a1f222cca75f3134a1e1f6a14359
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 14:33:19 -0400

    add javadoc for InformationNotFoundException

--- src/main/java/com/example/springbootminiproject/exception/InformationNotFoundException.java
+++ src/main/java/com/example/springbootminiproject/exception/InformationNotFoundException.java
@@ -3,9 +3,10 @@ package com.example.springbootminiproject.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**When InformationNotFoundException is thrown,  a response code of 404 not found is displayed, indicating that the information requested was not found.*/
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class InformationNotFoundException extends RuntimeException{
-    public InformationNotFoundException(String message){
+    public InformationNotFoundException(String message) {
         super(message);
     }
 }
