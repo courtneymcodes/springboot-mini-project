65f7692e79ace59c2fa561a966c764ce16527fe9
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 09:23:23 -0400

    declare Genre an Entity and Id field as primary key

--- src/main/java/com/example/springbootminiproject/model/Genre.java
+++ src/main/java/com/example/springbootminiproject/model/Genre.java
@@ -1,6 +1,11 @@
 package com.example.springbootminiproject.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Genre {
+    @Id
     private Long id;
     private String name;
     private String description;
