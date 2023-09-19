e3dec97c5f028124eabc24cc4a715765897969c2
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 03:23:42 -0400

    declare User model as an Entity and the Id field as the primary key

--- src/main/java/com/example/springbootminiproject/model/User.java
+++ src/main/java/com/example/springbootminiproject/model/User.java
@@ -1,6 +1,11 @@
 package com.example.springbootminiproject.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+    @Id
     private Long id;
     private String emailAddress;
     private String password;
