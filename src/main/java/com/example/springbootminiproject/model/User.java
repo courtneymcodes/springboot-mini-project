9578e15203219643d7435501464ceb38d7cb375c
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 03:08:06 -0400

    add getters and setters to User model

--- src/main/java/com/example/springbootminiproject/model/User.java
+++ src/main/java/com/example/springbootminiproject/model/User.java
@@ -13,4 +13,28 @@ public class User {
         this.emailAddress = emailAddress;
         this.password = password;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
