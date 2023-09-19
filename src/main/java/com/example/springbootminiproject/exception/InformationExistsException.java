255939e600511251f748440a768643018fcced01
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 16:28:25 -0400

    add constructor to InformationExistsException

--- src/main/java/com/example/springbootminiproject/exception/InformationExistsException.java
+++ src/main/java/com/example/springbootminiproject/exception/InformationExistsException.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.CONFLICT)
-public class InformationExistsException {
-
+public class InformationExistsException extends RuntimeException{
+    public InformationExistsException(String message) {
+        super(message);
+    }
 }
