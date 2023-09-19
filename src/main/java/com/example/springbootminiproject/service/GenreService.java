a279a7d0dd7a0e4097d03671c47f20faea4b1212
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 14:50:09 -0400

    add genreRepository to GenreService, inject with Autowired setter

--- src/main/java/com/example/springbootminiproject/service/GenreService.java
+++ src/main/java/com/example/springbootminiproject/service/GenreService.java
@@ -1,7 +1,14 @@
 package com.example.springbootminiproject.service;
 
+import com.example.springbootminiproject.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GenreService {
+    private GenreRepository genreRepository;
+    @Autowired
+    public void setGenreRepository(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
 }
