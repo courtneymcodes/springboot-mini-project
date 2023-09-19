1212ef0521fc4d65223b125717e9b746161b420b
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 15:31:46 -0400

    inject GenreService into GenreController

--- src/main/java/com/example/springbootminiproject/controller/GenreController.java
+++ src/main/java/com/example/springbootminiproject/controller/GenreController.java
@@ -1,9 +1,16 @@
 package com.example.springbootminiproject.controller;
 
+import com.example.springbootminiproject.service.GenreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class GenreController {
+    private GenreService genreService;
+    @Autowired
+    public void setGenreService(GenreService genreService) {
+        this.genreService = genreService;
+    }
 }
