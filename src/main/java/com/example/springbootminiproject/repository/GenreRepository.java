60055c7032bf5fd8591a4d8a9550b29d208b270a
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 14:03:13 -0400

    add inheritance from JpaRepository GenreRepository interface, add Repository annotation

--- src/main/java/com/example/springbootminiproject/repository/GenreRepository.java
+++ src/main/java/com/example/springbootminiproject/repository/GenreRepository.java
@@ -1,4 +1,9 @@
 package com.example.springbootminiproject.repository;
 
-public interface GenreRepository {
+import com.example.springbootminiproject.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
