d3235a38f9c8eb99f674f745cd5e45c983c4be7e
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 16:55:23 -0400

    add createGenre method to GenreController class

--- src/main/java/com/example/springbootminiproject/controller/GenreController.java
+++ src/main/java/com/example/springbootminiproject/controller/GenreController.java
@@ -3,9 +3,7 @@ package com.example.springbootminiproject.controller;
 import com.example.springbootminiproject.model.Genre;
 import com.example.springbootminiproject.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,15 @@ public class GenreController {
     @GetMapping(path = "/genres/")
     public List<Genre> getGenres(){
         return genreService.getGenres();
+    }
+
+    /**
+     * Returns the result of calling createGenre method on genreService instance when a POST request is made to /genres endpoint
+     * @param genreObject from request body
+     * @return created genre object
+     */
+    @PostMapping(path = "/genres/")
+    public Genre createGenre(@RequestBody Genre genreObject){
+        return genreService.createGenre(genreObject);
     }
 }
