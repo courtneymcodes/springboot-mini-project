c834d66b3246fc8c2a95896ff8b2782f89183ca0
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 09:09:32 -0400

    add secret and expiration values from dev properties

--- src/main/java/com/example/springbootminiproject/security/JWTUtils.java
+++ src/main/java/com/example/springbootminiproject/security/JWTUtils.java
@@ -1,7 +1,15 @@
 package com.example.springbootminiproject.security;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import java.util.logging.Logger;
 
 public class JWTUtils {
     Logger logger = Logger.getLogger(JWTUtils.class.getName());
+
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpirationMs;
 }
