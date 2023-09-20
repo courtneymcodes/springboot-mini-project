420dd296486471f187c8c3445f40b822de9ceaed
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 01:32:25 -0400

    add SecurityFilterChain to SecurityConfiguration

--- src/main/java/com/example/springbootminiproject/security/SecurityConfiguration.java
+++ src/main/java/com/example/springbootminiproject/security/SecurityConfiguration.java
@@ -3,7 +3,9 @@ package com.example.springbootminiproject.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -16,5 +18,22 @@ public class SecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Configures security chain for http requests
+     * @param http an HttoSecurity object
+     * @return a filter chain configuration
+     * @throws Exception if any issue arises
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeRequests().antMatchers("/auth/users","auth/users/login/","/auth/users/register/").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .and().csrf().disable()
+                .headers().frameOptions().disable();
+        return http.build();
     }
 }
