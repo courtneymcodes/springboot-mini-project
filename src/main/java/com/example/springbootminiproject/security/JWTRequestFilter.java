d2d4d957f4cf5c29e07071774022a6f4050a6062
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 09:49:16 -0400

    add parseJwt method

--- src/main/java/com/example/springbootminiproject/security/JWTRequestFilter.java
+++ src/main/java/com/example/springbootminiproject/security/JWTRequestFilter.java
@@ -2,7 +2,14 @@ package com.example.springbootminiproject.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class JWTRequestFilter extends OncePerRequestFilter {
 
@@ -16,4 +23,19 @@ public class JWTRequestFilter extends OncePerRequestFilter {
     public void setJwtUtils(JWTUtils jwtUtils) {
         this.jwtUtils = jwtUtils;
     }
+
+    /**
+     * Parses jwt token from the header of the http request
+     * @param request HttpSerletRequest with token and Autherization header
+     * @return jwtb token from header
+     */
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
+        return null;
+    }
+
+
 }
