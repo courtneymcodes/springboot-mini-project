5aa50fd11666de76ef6c72e17eee08c5891a1967
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 01:36:58 -0400

    add Lazy annotation to PasswordEncoder

--- src/main/java/com/example/springbootminiproject/service/UserService.java
+++ src/main/java/com/example/springbootminiproject/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.springbootminiproject.exception.InformationExistsException;
 import com.example.springbootminiproject.model.User;
 import com.example.springbootminiproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     @Autowired
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
