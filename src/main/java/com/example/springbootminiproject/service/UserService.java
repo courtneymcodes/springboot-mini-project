f49e7fb82d8115cae7ec86176767e2a6763abc86
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 00:48:55 -0400

    add PasswordEncoder bean in UserService

--- src/main/java/com/example/springbootminiproject/service/UserService.java
+++ src/main/java/com/example/springbootminiproject/service/UserService.java
@@ -4,14 +4,17 @@ import com.example.springbootminiproject.exception.InformationExistsException;
 import com.example.springbootminiproject.model.User;
 import com.example.springbootminiproject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User createUser(User userObject){
