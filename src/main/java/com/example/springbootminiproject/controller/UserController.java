c8c0eca2b77f203a4506270c1aa3f2067b6e337d
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 23:25:04 -0400

    implement createUser method on UserController

--- src/main/java/com/example/springbootminiproject/controller/UserController.java
+++ src/main/java/com/example/springbootminiproject/controller/UserController.java
@@ -1,7 +1,10 @@
 package com.example.springbootminiproject.controller;
 
+import com.example.springbootminiproject.model.User;
 import com.example.springbootminiproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +15,15 @@ public class UserController {
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    /**
+     * Returns the result of calling createUser method on userService when a POST request is made to /register endpoint
+     * @param userObject a User object from request body
+     * @return a User object
+     */
+    @PostMapping(path = "/register/")
+    public User createUser(@RequestBody User userObject){
+        return userService.createUser(userObject);
     }
 }
