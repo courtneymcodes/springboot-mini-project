d99b27c263674ff9f0265ded10b0e4d4685f494c
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 09:41:24 -0400

    MyUserDetailsService and JWTUtils to JWTRequestFilter

--- src/main/java/com/example/springbootminiproject/security/JWTRequestFilter.java
+++ src/main/java/com/example/springbootminiproject/security/JWTRequestFilter.java
@@ -1,7 +1,19 @@
 package com.example.springbootminiproject.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class JWTRequestFilter extends OncePerRequestFilter {
+
+    private MyUserDetailsService myUserDetailsService;
+    private JWTUtils jwtUtils;
+    @Autowired
+    public void setMyUserDetailsService(MyUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
+    @Autowired
+    public void setJwtUtils(JWTUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
 }
