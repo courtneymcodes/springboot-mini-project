a1c2e8462f2d5cab2b901e0abd6d3a9c6025e9ea
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 09:42:40 -0400

    add Service annotation

--- src/main/java/com/example/springbootminiproject/security/JWTUtils.java
+++ src/main/java/com/example/springbootminiproject/security/JWTUtils.java
@@ -2,11 +2,12 @@ package com.example.springbootminiproject.security;
 
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+@Service
 public class JWTUtils {
     Logger logger = Logger.getLogger(JWTUtils.class.getName());
 
