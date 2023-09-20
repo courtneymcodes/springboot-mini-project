5add8fe52da9e18fbffcc8b7ea8baadb9fcfb9f5
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-19 23:15:35 -0400

    add UserService to UserService with autowired setter

--- src/main/java/com/example/springbootminiproject/controller/UserController.java
+++ src/main/java/com/example/springbootminiproject/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.example.springbootminiproject.controller;
 
+import com.example.springbootminiproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "auth/users")
 public class UserController {
+    private UserService userService;
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
