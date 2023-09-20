8ef2daf12e2999a1128bac1dec9d6a10cb9ae042
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 01:48:17 -0400

    create MyUserDetails class that implements UserDetails, add method overide implementation

---
+++ src/main/java/com/example/springbootminiproject/security/MyUserDetails.java
@@ -0,0 +1,55 @@
+package com.example.springbootminiproject.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class MyUserDetails implements UserDetails {
+    private final User user;
+
+    public MyUserDetails(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
