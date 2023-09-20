1fbb467a4e7f47daba0c61c94b7cb89edd2c4287
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 09:33:07 -0400

    create JWTRequestFilter class that extends OncePerRequestFilter

---
+++ src/main/java/com/example/springbootminiproject/security/JWTRequestFilter.java
@@ -0,0 +1,7 @@
+package com.example.springbootminiproject.security;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JWTRequestFilter extends OncePerRequestFilter {
+}
