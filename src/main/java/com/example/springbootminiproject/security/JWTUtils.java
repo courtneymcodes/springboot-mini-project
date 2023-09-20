bbd8ead3015c382684fae89b1e486d4fd9d1ef82
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 09:05:56 -0400

    add JWTUtils class to security package, add logger

---
+++ src/main/java/com/example/springbootminiproject/security/JWTUtils.java
@@ -0,0 +1,7 @@
+package com.example.springbootminiproject.security;
+
+import java.util.logging.Logger;
+
+public class JWTUtils {
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
+}
