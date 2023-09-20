da37fcab918455ca8f8ffc4ffe4a4413f8a9cde9
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-20 00:44:34 -0400

    create a BCrypt bean

--- src/main/java/com/example/springbootminiproject/security/SecurityConfiguration.java
+++ src/main/java/com/example/springbootminiproject/security/SecurityConfiguration.java
@@ -1,10 +1,20 @@
 package com.example.springbootminiproject.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
 
+    /**
+     * Creates a BCryptPasswordEncoder bean that can be used to hash passwords
+     * @return an instance of BCryptPasswordEncoder
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
