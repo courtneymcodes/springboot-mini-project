8ce8c88d988ddbfc4d5ecc90e1e25844db13ddf9
Courtney <[200~63630567+courtneymcodes@users.noreply.github.com~>
2023-09-21 22:01:01 -0400

    add login endpoint to UserController

--- src/main/java/com/example/springbootminiproject/controller/UserController.java
+++ src/main/java/com/example/springbootminiproject/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.example.springbootminiproject.controller;
 
 import com.example.springbootminiproject.model.User;
+import com.example.springbootminiproject.model.request.LoginRequest;
+import com.example.springbootminiproject.model.response.LoginResponse;
 import com.example.springbootminiproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "auth/users")
@@ -25,5 +31,20 @@ public class UserController {
     @PostMapping(path = "/register/")
     public User createUser(@RequestBody User userObject){
         return userService.createUser(userObject);
+    }
+
+    /**
+     * Returns the result of calling loginUser method on userService to log in a user
+     * @param loginRequest object containing user credentials
+     * @return LoginResponse with token or error message
+     */
+    @PostMapping(path = "/login/")
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest){
+        Optional<String> jwtToken = userService.loginUser(loginRequest);
+        if (jwtToken.isPresent()) {
+            return ResponseEntity.ok(new LoginResponse(jwtToken.get()));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse("Authentication failed"));
+        }
     }
 }
